059902be2c564c6649d971411fdc3b8a65d8ba81
dhanraj mali <80768543+dhanraj644@users.noreply.github.com>
2025-10-09 19:06:07 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,14 +16,14 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-application-lake-pi.vercel.app/",
   },
   connectionStateRecovery: {},
 });
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://chat-application-lake-pi.vercel.app/",
   })
 );
 
